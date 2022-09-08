2151b777aa263ae8b5564d54b3dc70e309ec5fb1
sterparu <sterparu9@gmail.com>
2022-09-08 16:32:00 +0300

    first commit

---
+++ ManagementApp/ManagementApp/src/main/java/com/example/ManagementApp/ManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementAppApplication.class, args);
+	}
+
+}
---
+++ ManagementApp/ManagementApp/src/test/java/com/example/ManagementApp/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
